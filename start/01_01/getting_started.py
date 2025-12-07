8dbc858d84eb3226dc5b15808af45d8400351ddf
Anusha Rai <158173561+Anusharai20@users.noreply.github.com>
2025-12-07 16:12:08 +0000

    learning properties and constructors

--- start/01_01/getting_started.py
+++ start/01_01/getting_started.py
@@ -1,3 +1,28 @@
 """
 Python Object-Oriented Programming Class Example
 """
+
+class Card:
+    # initialize card with value and suit and it is also a constructor    
+    def __init__(self, value, suit):
+        # attributes assignment
+        self.value = value
+        self.suit = suit
+
+# function/method to represent the card as a string
+    def __repr__(self):
+        # string representation of the card
+        return f"{self.value} of {self.suit}"
+
+
+if __name__ == "__main__":
+    # create two card objects
+    card1 = Card("Ace", "Spades") 
+    card2 = Card("Queen", "Hearts")
+    card3 = Card("King", "Daimonds")
+    card4 = Card("Six", "Spades")
+
+    print(card1)
+    print(card2)
+    print(card3)
+    print(card4)
--- start/02_01/properties.py
+++ start/02_01/properties.py
@@ -1,3 +1,18 @@
 """
 Python class properties
 """
+
+
+class Rectangle:
+    def __init__(self, base, height):
+        self.base = base
+        self._height = height
+
+
+new_rectangle = Rectangle(12, 10)
+print(new_rectangle.base)      # Output: 12
+print(new_rectangle._height)   # Output: 10
+# Modifying the base attribute
+new_rectangle.base = 15
+print()
+print(new_rectangle.base)      # Output: 15
